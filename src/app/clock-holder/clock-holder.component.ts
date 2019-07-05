4851d15fad10063678b36b1ed5a576b2e420463e
Oliver Pavey <42508563+OliverPavey@users.noreply.github.com>
2019-07-05 22:37:26 +0100

    Wire up 'Force Update' button to request clock update from server.

--- src/app/clock-holder/clock-holder.component.ts
+++ src/app/clock-holder/clock-holder.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ClockComponent } from '../clock/clock.component';
 
 @Component({
   selector: 'app-clock-holder',
@@ -9,7 +10,12 @@ export class ClockHolderComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('clock') clock: ClockComponent;
+
   ngOnInit() {
   }
 
+  updateButtonClick(event: Event) {
+    this.clock.forceUpdate();
+  }
 }
