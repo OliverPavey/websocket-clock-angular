f0a60f17117c4f9a4bff1338e1c7f036ce73251c
Oliver Pavey <42508563+OliverPavey@users.noreply.github.com>
2019-07-05 22:24:06 +0100

    Add code to connect to web-socket and update clock display when message received from server.

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-clock',
@@ -9,7 +11,34 @@ export class ClockComponent implements OnInit {
 
   constructor() { }
 
+  clockText: String = '??:??.??';
+  stompClient = null;
+
   ngOnInit() {
+    console.log('connecting');
+    // Connect to websocket '/websocket-clock' (on same server that delivered webpage)
+    const socket = new SockJS('http://localhost:8080/websocket-clock');
+    const appClock = this;
+    appClock.stompClient = Stomp.over(socket);
+    appClock.stompClient.connect({},
+      function(frame: any) {
+        // Subscribe to '/topic/clock' update broadcasts
+        appClock.stompClient.subscribe('/topic/clock',
+          function(message: { body: string; }) {
+            appClock.updateClock(JSON.parse(message.body).time);
+          });
+        // Force immediate update on first connection
+        appClock.forceUpdate();
+      });
+    console.log('connection done');
+  }
+
+  updateClock(time: String) {
+    this.clockText = time;
+  }
+
+  forceUpdate() {
+    this.stompClient.send('/app/clock', {}, JSON.stringify({}));
   }
 
 }
